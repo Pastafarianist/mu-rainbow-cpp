57a33cab7a0b84fc2d3585c6f52c6351b4476585
Lionel Miller <delonorm@gmail.com>
2017-10-08 19:14:36 +0300

    Use unique_ptr instead of new/delete

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 #include "storage.h"
 
 
 int main() {
-    auto *storage = new Storage();
+    auto storage = std::make_unique<Storage>();
     std::vector<State> starting_states = make_starting_states();
     for (std::size_t i = 0; i < starting_states.size(); i++) {
         std::cout << "Processing state " << (i + 1) << "/" << starting_states.size() << std::endl;
         mark_states_reachable_from(starting_states[i], *storage);
     }
-    storage->dump("reachability.bin");
-    delete storage;
+    const std::string dump_filename = "reachability.bin";
+    std::cout << "Done! Dumping result to " << dump_filename << std::endl;
+    storage->dump(dump_filename);
     return 0;
 }
